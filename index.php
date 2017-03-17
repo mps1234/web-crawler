a4d3af0b9012b4bb5ea83330569a7d67f9ae0b3d
Mayur Pathak <mayur.pathak52@gmail.com>
2017-03-17 12:29:20 +0530

    Crawler completed

--- index.php
+++ index.php
@@ -1,28 +1,90 @@
 <?php
 
-	$start = "http://localhost/Crawler/links.php";	//link of the web page you want to crawl
+	$start = "http://www.akgec.in/bdc17";	//link of the web page you want to crawl
 
+	$already_crawled = array();
+	$crawling = array();
+
+	function get_details($url){
+		$options = array('http'=>array('method' =>"GET", 'headers'=>"User-agent: howBot/0.1\n"));
+		$context = stream_context_create($options);
+
+		$doc = new DOMdocument();
+		@$doc->loadHTML(@file_get_contents($url, false, $context));
+
+		$title = $doc->getElementsByTagName("title");
+		$title = @$title->item(0)->nodeValue;
+		//echo $title."\n";
+
+		$description = "";
+		$keywords = "";
+
+		$metas = $doc->getElementsByTagName("meta");
+		for($i = 0; $i < $metas->length; $i++){
+			$meta = $metas->item($i);
+
+			if(strtolower($meta->getAttribute("name")) == "description"){
+				$description = $meta->getAttribute("content");
+			}
+			if(strtolower($meta->getAttribute("name")) == "keywords"){
+				$keywords = $meta->getAttribute("content");
+			}
+		}
+
+		return '{ "Title": "'.str_replace("\n", "", $title).'", "Description": "'.str_replace("\n", "", $description).'", "Keywords": "'.str_replace("\n", "", $keywords).'", "URL": "'.$url.'"},';
+	}
 
 	//function to crawl the web page
 	function follow_links($url){
 
+		global $already_crawled;
+		global $crawling;
+
+		$options = array('http'=>array('method' =>"GET", 'headers'=>"User-agent: howBot/0.1\n"));
+		$context = stream_context_create($options);
+
 		$doc = new DOMdocument();
-		$doc->loadHTML(file_get_contents($url));
+		@$doc->loadHTML(@file_get_contents($url, false, $context));
 
 		$linklist = $doc->getElementsByTagName("a");
 
 		foreach ($linklist as $link) {
 
-			$l = $link->getAttribute("href")."\n";
+			$l = $link->getAttribute("href");
 
 			if(substr($l, 0, 1) == "/" && substr($l, 0, 2) != "//"){
-
-				$l = $l;
+				$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].$l;
+			}	
+			else if(substr($l, 0, 2) == "//"){
+				$l = parse_url($url)["scheme"].":".$l;
+			}
+			else if(substr($l, 0, 2) == "./"){
+				$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).substr($l,1);
+			}
+			else if(substr($l,0, 1) == "#"){
+				$l = parse_url($url)["scheme"]."://".parse_url($url)["host"].parse_url($url)["path"].$l;	
+			}
+			else if(substr($l, 0, 3) == "../"){
+				$l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+			}
+			else if(substr($l, 0, 11) == "javascript:"){
+				continue;
+			}
+			else if(substr($l, 0, 5) != "https" && substr($l, 0, 4) != "http"){
+				$l = parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;	
 			}
 
-			echo $l."\n";
+			if(!in_array($l, $already_crawled)){
+				$already_crawled[] = $l;
+				$crawling[] = $l;
+				echo get_details($l)."\n";
+				//echo $l."\n";	
+			}
 
-
+		}
+		array_shift($crawling);
+		foreach ($crawling as $site){
+			follow_links($site);
 		}
 
 
@@ -31,5 +93,6 @@
 
 	//Calling the function which will crawl
 	follow_links($start);
+	//print_r($already_crawled);
 
 ?>
--- links.php
+++ links.php
@@ -1,11 +1,21 @@
 
 
 <a href="https://google.com">Go to google</a><br/>
+
 <a href="test.php">Test Page</a><br/>
-<a href="//www.youtube.com/upload">Youtube</a><br/>
-<a href="../../anotherpage.php">Another page</a><br/>
-<a href="/test/another/../../anotherpage.php">Another page</a><br/>
-<a href="#anchor">Link with anchor</a><br/>
+
+<a href="//www.youtube.com/upload">Youtube</a><br/>	<!--  Done no 2 -->
+
+<a href="../../anotherpage.php">Another page</a><br/><!-- Done no 5 -->
+
+<a href="/test/another/../../anotherpage.php">Another page</a><br/><!-- Done no 1-->
+
+<a href="#anchor">Link with anchor</a><br/><!-- Done no 4 -->
+
 <a href="//www.fjlds.com/somepage.php">Go to google</a><br/>
-<a href="./moore.php">Go to google</a><br/>
+
+<a href="./moore.php">Go to google</a><br/><!-- Done no 3 -->
+
+<a href="/even-more.php">Even More</a><br/><!-- Done no 1 -->
+
 <a href="javascript:d">Javascript link </a>
